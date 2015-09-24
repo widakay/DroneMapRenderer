b3744a612c66332d3b7bef56783ef6050c962292
widdakay <erik@yoerik.com>
2015-09-24 10:57:08 -0700

    initial commit

---
+++ data/test.js
@@ -0,0 +1,199 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "odm_textured_model.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 100037,
+        "faces"         : 199905,
+        "normals"       : 0,
+        "uvs"           : 599715,
+        "materials"     : 13
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "texture_0",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.0, 0.0, 0.0],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapDiffuse" : "texture_0.jpg",
+	"opacity" : 0.0,
+	"specularCoef" : 200.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 15597568,
+	"DbgIndex" : 1,
+	"DbgName" : "texture_1",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.0, 0.0, 0.0],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapDiffuse" : "texture_1.jpg",
+	"opacity" : 0.0,
+	"specularCoef" : 200.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 60928,
+	"DbgIndex" : 2,
+	"DbgName" : "texture_2",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.0, 0.0, 0.0],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapDiffuse" : "texture_2.jpg",
+	"opacity" : 0.0,
+	"specularCoef" : 200.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 238,
+	"DbgIndex" : 3,
+	"DbgName" : "texture_3",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.0, 0.0, 0.0],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapDiffuse" : "texture_3.jpg",
+	"opacity" : 0.0,
+	"specularCoef" : 200.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 15658496,
+	"DbgIndex" : 4,
+	"DbgName" : "texture_4",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.0, 0.0, 0.0],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapDiffuse" : "texture_4.jpg",
+	"opacity" : 0.0,
+	"specularCoef" : 200.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 61166,
+	"DbgIndex" : 5,
+	"DbgName" : "texture_5",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.0, 0.0, 0.0],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapDiffuse" : "texture_5.jpg",
+	"opacity" : 0.0,
+	"specularCoef" : 200.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 15597806,
+	"DbgIndex" : 6,
+	"DbgName" : "texture_6",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.0, 0.0, 0.0],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapDiffuse" : "texture_6.jpg",
+	"opacity" : 0.0,
+	"specularCoef" : 200.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 14968288,
+	"DbgIndex" : 7,
+	"DbgName" : "texture_7",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.0, 0.0, 0.0],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapDiffuse" : "texture_7.jpg",
+	"opacity" : 0.0,
+	"specularCoef" : 200.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 7078673,
+	"DbgIndex" : 8,
+	"DbgName" : "texture_8",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.0, 0.0, 0.0],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapDiffuse" : "texture_8.jpg",
+	"opacity" : 0.0,
+	"specularCoef" : 200.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 1458591,
+	"DbgIndex" : 9,
+	"DbgName" : "texture_9",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.0, 0.0, 0.0],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapDiffuse" : "texture_9.jpg",
+	"opacity" : 0.0,
+	"specularCoef" : 200.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 3668136,
+	"DbgIndex" : 10,
+	"DbgName" : "texture_10",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.0, 0.0, 0.0],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapDiffuse" : "texture_10.jpg",
+	"opacity" : 0.0,
+	"specularCoef" : 200.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 499914,
+	"DbgIndex" : 11,
+	"DbgName" : "texture_11",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.0, 0.0, 0.0],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapDiffuse" : "texture_11.jpg",
+	"opacity" : 0.0,
+	"specularCoef" : 200.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 8478454,
+	"DbgIndex" : 12,
+	"DbgName" : "non_visible_faces_texture",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.0, 0.0, 0.0],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapDiffuse" : "non_visible_faces_texture.jpg",
+	"opacity" : 0.0,
+	"specularCoef" : 200.0,
+	"transparent" : true
+	}],
+
+    "buffers": "test.bin"
+
+}
