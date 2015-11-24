24947526bf061efd5cd9bb30ed8db0112890030b
widdakay <erik@yoerik.com>
2015-11-24 09:38:42 -0800

    New mesh loader system

--- js/MeshLoader.js
+++ js/MeshLoader.js
@@ -1,14 +1,28 @@
 
-function addMesh(url) {
+var largeMesh;
+function addMesh(basepath, info) {
     loadingMesh = true;
+    
+    loadingMesh = false;
+    return;
+    var url = basepath + info["meshURL"];
     var loader = new THREE.ColladaLoader();
     loader.options.convertUpAxis = true;
     loader.load(url, function(collada) {
 
         largeMesh = collada.scene;
-        largeMesh.scale.x = largeMesh.scale.y = largeMesh.scale.z = 40;
-        largeMesh.rotation.x = -3 / 2;
-        largeMesh.position.y = 80;
+        largeMesh.scale.x = largeMesh.scale.y = largeMesh.scale.z = info["scale"];//40;
+
+        largeMesh.rotation.x = info["rotation"][0];//-3 / 2;
+        largeMesh.rotation.y = info["rotation"][1];
+        largeMesh.rotation.z = info["rotation"][2];
+
+
+        largeMesh.position.x = info["offset"][0];
+        largeMesh.position.y = info["offset"][1];//80;
+        largeMesh.position.z = info["offset"][2];
+        console.log("mesh position", info["offset"])
+
         largeMesh.updateMatrix();
 
         scene.add(largeMesh);
